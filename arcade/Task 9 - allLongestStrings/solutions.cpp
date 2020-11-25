b2d0ff4e2199a80ccd6ec8c180107170ad386e97
Rico Trebeljahr <25333032+trebeljahr@users.noreply.github.com>
2020-11-25 10:33:21 +0100

    Add arcade folder

---
+++ arcade/Task 9 - allLongestStrings/solutions.cpp
@@ -0,0 +1,20 @@
+vector<string> allLongestStrings(vector<string> inputArray)
+{
+    vector<string> out = {};
+    size_t max = 0;
+    for (auto line : inputArray)
+    {
+        if (line.length() > max)
+        {
+            max = line.length();
+        }
+    }
+    for (auto line : inputArray)
+    {
+        if (line.length() == max)
+        {
+            out.insert(out.end(), line);
+        }
+    }
+    return out;
+}
